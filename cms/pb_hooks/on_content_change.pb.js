b16cf0e1c5ac75d78d796f2f4c76bf3b15a8303c
hectorsector <hector@hectorsector.com>
2026-03-12 13:30:41 -0400

    fix hook

--- cms/pb_hooks/on_content_change.pb.js
+++ cms/pb_hooks/on_content_change.pb.js
@@ -1,27 +1,44 @@
 onRecordAfterCreateSuccess((e) => {
-  triggerHugoBuild();
-  e.next();
-});
-
-onRecordAfterUpdateSuccess((e) => {
-  triggerHugoBuild();
-  e.next();
-});
-
-onRecordAfterDeleteSuccess((e) => {
-  triggerHugoBuild();
-  e.next();
-});
-
-function triggerHugoBuild() {
+  $app.logger().info("onRecordAfterCreateSuccess fired");
   $http.send({
     method: "POST",
     url: "https://api.github.com/repos/hectorsector/oya-test/actions/workflows/hugo.yml/dispatches",
     headers: {
-      Authorization: "Bearer " + __ENV["GITHUB_TOKEN"],
+      Authorization: "Bearer " + $os.getenv("GITHUB_TOKEN"),
       Accept: "application/vnd.github+json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ ref: "main" }),
   });
-}
+  e.next();
+});
+
+onRecordAfterUpdateSuccess((e) => {
+  $app.logger().info("onRecordAfterUpdateSuccess fired");
+  $http.send({
+    method: "POST",
+    url: "https://api.github.com/repos/hectorsector/oya-test/actions/workflows/hugo.yml/dispatches",
+    headers: {
+      Authorization: "Bearer " + $os.getenv("GITHUB_TOKEN"),
+      Accept: "application/vnd.github+json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ref: "main" }),
+  });
+  e.next();
+});
+
+onRecordAfterDeleteSuccess((e) => {
+  $app.logger().info("onRecordAfterDeleteSuccess fired");
+  $http.send({
+    method: "POST",
+    url: "https://api.github.com/repos/hectorsector/oya-test/actions/workflows/hugo.yml/dispatches",
+    headers: {
+      Authorization: "Bearer " + $os.getenv("GITHUB_TOKEN"),
+      Accept: "application/vnd.github+json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ref: "main" }),
+  });
+  e.next();
+});
